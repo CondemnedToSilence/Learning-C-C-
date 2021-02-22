881cc8cd807c9a4d3bd91b7b086221fe83e0ccf0
CondemnedToSilence <71749839+CondemnedToSilence@users.noreply.github.com>
2021-02-22 21:44:29 +0530

    Update basic_data_types.c

--- basic_data_types.c
+++ basic_data_types.c
@@ -2,10 +2,13 @@
 int main()
 {
     int a=4,b=17;
+    const int pi = 3.14;
+    //const means the variable cant be changed once set
     float c = 72.435;
     char letter = 'F';
     //notice the single quotes for char
     int d = a+b;
+    printf("%d \n",pi);
     printf("%d \n",d);
     printf("%f \n",c);
     printf("%c \n",letter);
